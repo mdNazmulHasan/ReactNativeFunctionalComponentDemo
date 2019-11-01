c9e2cdf66220b8840200fc1857de6a7e47607f5a
mdNazmulHasan <najmul.hasan10@gmail.com>
2019-11-01 15:17:43 +0600

    header right icon added

--- src/screen/Business.js
+++ src/screen/Business.js
@@ -1,10 +1,15 @@
 import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import {View, Text, StyleSheet, Image} from 'react-native';
+import avatar from '../../assets/avatar/avatar.png';
+
 const styles = StyleSheet.create({
   headerStyle: {
     flex: 1,
     fontFamily: 'SF-Pro-Display-Regular',
     textAlign: 'center',
+  },
+  imageStyle: {
+    marginRight: 20,
   },
 });
 
@@ -18,6 +23,7 @@ const Business = props => {
 Business.navigationOptions = () => {
   return {
     headerTitle: <Text style={styles.headerStyle}>Business</Text>,
+    headerRight: <Image source={avatar} style={styles.imageStyle} />,
   };
 };
 
